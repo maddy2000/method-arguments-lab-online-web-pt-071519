668efb70c0fa2a68fd05f9551ca8b9ce5319ef83
Madelyn Deshield <madelynwallop@gmail.com>
2019-07-18 01:47:51 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end  
+
+def introduction(madelyn, ruby)
+  puts "hi, my name is #{madelyn} and i am learning to program in #{ruby}."
+end  
